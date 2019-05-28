5d250045c1f419551f0854886dbacfd201e48a81
Joe Siltberg <git@joesiltberg.se>
2019-05-28 13:43:23 +0200

    New implementation of find_variables()
    Fixes #24

--- src/json_template_parser.cpp
+++ src/json_template_parser.cpp
@@ -17,92 +17,78 @@
  */
 
 #include "json_template_parser.hpp"
-#include <algorithm>
-#include <vector>
+#include <regex>
 #include <iostream>
-#include "utility/utils.hpp"
 
 namespace JSONTemplateParser {
 
-void skip_ws(iter& cur, iter end) {
-    while (cur != end && (*cur == ' ' || *cur == '\t')) {
-        ++cur;
+using namespace std;
+
+bool handle_switch(const string& str, string& variable) {
+    smatch m;
+    if (regex_match(str, m, regex("\\$\\{[[:space:]]*switch[[:space:]]*((\\w|\\.)+)[[:space:]](.|\n)*\\}"))) {
+        variable = m[1].str();
+        return true;
+    }
+    else {
+        return false;
     }
 }
 
-std::set<std::string> find_variables(iter start, iter end) {
-
-    auto cur = start;
-    
-    std::set<std::string> var_set;
-
-    while (cur != end) {
-        iter var_start = std::find(cur, end, '$');
-        iter array_start = std::find(cur, end, '[');
-
-        if (array_start < var_start) {
-            iter array_end = std::find(array_start, end, ']');
-            if (array_end != end) {
-                size_t for_pos = std::string(array_start, array_end).find("${for");
-                if (for_pos == std::string::npos) {
-                    cur = array_start + 2;
-                    continue;
-                }
-                cur = array_start + for_pos;
-                if (cur < array_end) {
-                    cur = cur + std::string(cur, array_end).find("in");
-                    if (cur < array_end) {
-                        cur += 2;
-                        skip_ws(cur, end);
-
-                        std::vector<std::string> tmp = string_to_vector({cur, std::find(cur, array_end, '}')});
-                        var_set.insert(tmp.begin(), tmp.end());
-                    }
-                }
-                cur = array_end + 1;
-                continue;
+bool handle_for_header(const string& str, set<string>& variables) {
+    smatch m;
+    if (regex_match(str, m, regex("\\$\\{[[:space:]]*for[[:space:]](.|\\n)*?[[:space:]]in[[:space:]](([[:space:]]*(\\w|\\.)+[[:space:]]*)*)\\}"))) {
+        if (m.size() > 2) {
+            string variable_sequence = m[2].str();
+            istringstream is(variable_sequence);
+            string var;
+            while (is >> var) {
+                variables.insert(var);
             }
         }
-        cur = var_start;
-        if (cur != end) {
-            if (*(cur + 1) != '{') {
-                // a dollar sign without curly brace is just a dollar sign
-                cur++;
-                continue;
-            }
-            /** see if it's a switch */
-            size_t switch_pos = std::string(cur, end).find("switch");
-            if (switch_pos != std::string::npos) {
-                iter block_end = std::find(cur, end, '}');
-                if (cur + switch_pos < block_end) {
-                    cur += std::string("{switch ").length();
-                    skip_ws(cur, end);
-                    iter var_end = cur + std::string(cur, block_end).find_first_of(" \n\t}");
-                    var_set.insert({cur, var_end});
-                    continue;
-                }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-            }
-
-            cur += 2;
-            skip_ws(cur, end);
-            auto v_end = std::find(cur, end, '}');
-            if (v_end != end) {
-                std::string variable(cur, v_end);
-                if (variable.find_first_of(" \n\t") != std::string::npos) {
-                    std::cerr << R"(syntax error "${" needs corresponding "}")" << std::endl;
-                }
-
-                var_set.insert(variable);
-                cur = v_end;
-            } else {
-                std::cerr << R"(syntax error "${" needs corresponding "}")" << std::endl;
-                return std::set<std::string>();
-            }
+bool handle_variable(const string& str, string& variable) {
+    smatch m;
+    if (regex_match(str, m, regex("\\$\\{[[:space:]]*((\\w|\\.)+)[[:space:]]*\\}"))) {
+        if (m.size() > 1 && m[1].str() != "end") {
+            variable = m[1].str();
+            return true;
         }
     }
+    return false;
+}
 
-    return var_set;
+set<string> find_variables(iter start, iter end) {
+    string var;
+    set<string> vars;
+    smatch m;
+    bool found = regex_search(start, end, m, regex("\\$\\{(.|\\n)*?\\}"));
+    if (!found) {
+        return {};
+    }
+    
+    auto result = find_variables(m.suffix().first, m.suffix().second);
+    
+    if (handle_switch(m[0].str(), var)) {
+        result.insert(var);
+    }
+    else if (handle_for_header(m[0].str(), vars)) {
+        result.insert(vars.begin(), vars.end());
+    }
+    else if (handle_variable(m[0].str(), var)) {
+        result.insert(var);
+    }
+    else {
+        // ${end}, unknown ${...}-sequence or a variable which shouldn't be
+        // included (for instance iteration variables like ${$i}), just ignore it
+    }
+    return result;
 }
 
 }
--- src/tests/json_template_parser_tests.cpp
+++ src/tests/json_template_parser_tests.cpp
@@ -26,14 +26,42 @@ TEST_CASE("Basic variable expansion") {
 
 TEST_CASE("Switch expansion") {
     REQUIRE(find_variables("${switch foo case \"a\": \"b\" case \"c\": \"d\" default: \"e\"}") == VariableSet{"foo"});
+
+    REQUIRE(find_variables("{\n"\
+                           "\t'a' : 'b',\n"\
+                           "\t'c' : '${ switch\td.e\n"\
+                           "                  case \"a\" : \"b\"\n"\
+                           "                  default : \"c\"\n"\
+                           "}'") == VariableSet{"d.e"});
 }
 
 
 TEST_CASE("Iterative expansion") {
-    REQUIRE(find_variables("${for $e in email}\n"\
+    REQUIRE(find_variables("${for $e in email}\n"  \
                            "{\n"\
-                           " 'value:' '${$e}\n"\
-                           " 'display:' '${displayName}\n"\
+                           " 'value': '${$e}'\n"\
+                           " 'displa:y' '${displayName}'\n"\
                            "},\n"\
                            "${end}") == VariableSet{"email", "displayName"});
+
+    REQUIRE(find_variables("${for $a $b in a b.c }\n"\
+                           "{\n"\
+                           " 'value': '${$a}'\n"\
+                           " 'display': '${$b}'\n"\
+                           "},\n"\
+                           "${end}") == VariableSet{"a", "b.c"});
+}
+
+TEST_CASE("Nested interative expansion") {
+    REQUIRE(find_variables("${for $a in arr}\n"\
+                           "${ruben}\n"\
+                           "${ for $b $c in simeon levy.juda }\n"\
+                           "${dan}\n"\
+                           "${end }\n"\
+                           "${ naftali}${gad}\n"\
+                           "${ end}\n"\
+                           "${switch aser case \"isaskar\" : \"sebulon\"}\n"\
+                           "josef : ${benjamin}") ==
+            VariableSet{"arr", "ruben", "simeon", "levy.juda", "dan",
+                    "naftali", "gad", "aser", "benjamin"});
 }
