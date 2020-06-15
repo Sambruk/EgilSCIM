d8d295b0c43fa886a894253e1642a4a5747d7e6a
Joe Siltberg <git@joesiltberg.se>
2020-06-15 10:26:51 +0200

    Adds an example post processing plugin.
    The echo post processing plugin simply passes the text through
    without changes.

---
+++ plugins/pp/echo/echo.c
@@ -0,0 +1,41 @@
+/**
+ *  This file is part of the EGIL SCIM client.
+ *
+ *  Copyright (C) 2017-2020 Föreningen Sambruk
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <pp_interface.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int echo_init(int count, char **vars, char **values) {
+    return 0;
+}
+
+extern int echo_include(const char *type) {
+    return PP_PROCESS_TYPE;
+}
+
+extern char *echo_process(const char *type, const char *input) {
+    return strdup(input);
+}
+
+extern void echo_free(void *ptr) {
+    free(ptr);
+}
+
+void echo_exit() {
+}
